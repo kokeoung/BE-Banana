73a4767d453432011d6d2e4acaa803931117f0cc
조준희 <soyre1928@gmail.com>
2025-04-21 12:23:48 +0900

    최신정보

--- src/main/java/com/kh/banana/controller/MyController.java
+++ src/main/java/com/kh/banana/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.kh.banana.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.banana.service.MypageService;
 
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RequiredArgsConstructor
@@ -21,5 +23,10 @@ public class MyController {
 	public ResponseEntity<?> findUser(@PathVariable(value = "userId")String userId) {
 		return service.findUser(userId);
 	}
-	
+	@PostMapping("/api/my/change/{userId}")
+	public ResponseEntity<?> updateUser(
+			
+		    @RequestParam(required = false) MultipartFile userProfile) {
+		return service.updateUser();
+	}
 }
